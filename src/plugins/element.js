587d2d4801f804a1e906d1b5b1f9df156364cbff
1534266 <1371086135@qq.com>
2021-01-30 19:30:17 +0800

    user

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,9 +1,5 @@
 import Vue from 'vue'
-// import { Button } from 'element-ui'
-// import { Form, FormItem } from 'element-ui'
-import { Button, Form, FormItem, Input, Message, Container, Header, Aside, Main, Menu, Submenu, MenuItem, Breadcrumb, BreadcrumbItem, Card, Row, Col, Table, TableColumn, Switch, Tooltip, Pagination, Dialog, MessageBox, Tag } from 'element-ui'
-// 弹框提示组件
-// import { Message } from 'element-ui'
+import { Button, Form, FormItem, Input, Message, Container, Header, Aside, Main, Menu, Submenu, MenuItem, Breadcrumb, BreadcrumbItem, Card, Row, Col, Table, TableColumn, Switch, Tooltip, Pagination, Dialog, MessageBox, Tag, Tree, Select, Option } from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
@@ -24,12 +20,13 @@ Vue.use(Col)
 Vue.use(Table)
 Vue.use(TableColumn)
 Vue.use(Switch)
-Vue.use(Switch)
 Vue.use(Tooltip)
 Vue.use(Pagination)
 Vue.use(Dialog)
 Vue.use(Tag)
+Vue.use(Tree)
+Vue.use(Select)
+Vue.use(Option)
 
-// Vue.use(Submenu)
-// 把弹框组件挂在到vue的原型对象上,每个组件都可以直接通过this使用
 Vue.prototype.$message = Message
+Vue.prototype.$confirm = MessageBox.confirm
